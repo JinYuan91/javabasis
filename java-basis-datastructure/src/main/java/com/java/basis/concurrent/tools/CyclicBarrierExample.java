fd0dbe2db2b05846d935f205f77ca54ac61b7ab7
jinyuan91 <641652097@qq.com>
2020-02-28 12:49:22 +0800

    [add]add CyclicBarrier Example

--- java-basis-datastructure/src/main/java/com/java/basis/concurrent/tools/CyclicBarrierExample.java
+++ java-basis-datastructure/src/main/java/com/java/basis/concurrent/tools/CyclicBarrierExample.java
@@ -1,4 +1,50 @@
 package com.java.basis.concurrent.tools;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class CyclicBarrierExample {
+
+
+
+
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(2, new CyclicBarrierRunExample());
+        Thread otherRunExampleA = new Thread(new OtherRunExample(cyclicBarrier, "A"));
+        Thread otherRunExampleB = new Thread(new OtherRunExample(cyclicBarrier, "B"));
+        otherRunExampleA.start();
+        otherRunExampleB.start();
+    }
+
+    static class CyclicBarrierRunExample implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("CyclicBarrierRun Example");
+        }
+    }
+
+    static class OtherRunExample implements Runnable {
+        private CyclicBarrier cyclicBarrier;
+        private String name;
+
+        public OtherRunExample(CyclicBarrier cyclicBarrier, String name) {
+            this.cyclicBarrier = cyclicBarrier;
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(name + " begin run input barrier");
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+            System.out.println(name + "go on run");
+
+        }
+    }
 }
