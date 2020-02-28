844136235b9e8b3740b231e32c423d790c1dd376
jinyuan91 <641652097@qq.com>
2020-02-28 17:35:08 +0800

    [mod]countdownlatch example add

--- java-basis-datastructure/src/main/java/com/java/basis/concurrent/tools/CountDownLatchExample.java
+++ java-basis-datastructure/src/main/java/com/java/basis/concurrent/tools/CountDownLatchExample.java
@@ -1,4 +1,66 @@
 package com.java.basis.concurrent.tools;
 
+import java.util.concurrent.CountDownLatch;
+
 public class CountDownLatchExample {
+    private static CountDownLatch countDownLatch=new CountDownLatch(3);
+
+    public static void main(String[] args)
+    {
+        for(int i=0;i<3;i++)
+        {
+            AcquireCount acquireCount=new AcquireCount(countDownLatch);
+            acquireCount.start();
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for(int i=0;i<3;i++)
+        {
+            ReleaseCount releaseCount=new ReleaseCount(countDownLatch);
+            releaseCount.start();
+        }
+    }
+
+
+    static class ReleaseCount extends Thread
+    {
+        private CountDownLatch countDownLatch;
+        public ReleaseCount(CountDownLatch countDownLatch)
+        {
+            this.countDownLatch=countDownLatch;
+        }
+        @Override
+        public void run() {
+
+            System.out.println(this.getId()+"-Release over");
+            countDownLatch.countDown();
+        }
+    }
+
+    static class AcquireCount extends Thread
+    {
+        private CountDownLatch countDownLatch;
+        public AcquireCount(CountDownLatch countDownLatch)
+        {
+            this.countDownLatch=countDownLatch;
+        }
+        @Override
+        public void run() {
+            System.out.println(this.getId()+"-Acquire begin");
+
+                try {
+                    countDownLatch.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(this.getId()+"-Acquire over");
+
+        }
+    }
+
+
 }
